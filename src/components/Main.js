84a8e315a6f22fc3e9cf74e439e52d8ba1f8b6e0
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-06 09:45:25 -0600

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,18 @@ import React from "react";
 
 export default function Main() {
     return (
-        <h1>Main component</h1>
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by FaceBook</li>
+                <li>Powers thousands of enterprise apps including mobile apps</li>
+
+            </ul>
+        </main>
+        
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
-import logo from "../logo.svg";
+import logo from "../images/react-icon.png";
 
 function Navbar() {
     return (
         <nav>
-            <img src={ logo } alt="" width="130px" className="nav--icon"/>
+            <img src={ logo } alt=""  className="nav--icon"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
