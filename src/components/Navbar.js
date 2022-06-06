798047d285debde4f8df389e9cc358e05d4a31e5
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-05 23:41:10 -0600

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import logo from "../logo.svg";
 function Navbar() {
     return (
         <nav>
-            <img src={ logo } alt="" width="100px"/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={ logo } alt="" width="130px" className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
