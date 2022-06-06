a24e53d1abe5622941944d2dadea890ab03d7da5
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-06 17:23:26 -0600

    update

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -2,22 +2,29 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 import './App.css';
-// import images from "./images";
 
+console.log(data);
 function App() {
+ 
+  const cards = data.map(item => {
+    return (
+      <Card
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+        />
+    )
+  })
   return (
     <div>
       <Navbar />
       {/* <Hero /> */}
-      <Card 
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessson with Katie faferes"
-        price={130}
-      />
+      {cards}
     </div>
   );
 }
