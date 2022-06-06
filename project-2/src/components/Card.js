0b7fd994362189159c08cd97f846e85a0f1c0daa
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-06 16:27:31 -0600

    update

--- project-2/src/components/Card.js
+++ project-2/src/components/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Card(props) {
-    console.log(props);
     return (
         <div className="card">
             <img src={`assets/images/${props.img}`} alt="" className="card--image" />
