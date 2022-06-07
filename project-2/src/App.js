4aee1a674c36890bc2050030226831a565cddebe
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-06 19:54:28 -0600

    update

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -11,13 +11,16 @@ function App() {
   const cards = data.map(item => {
     return (
       <Card
-          img={item.coverImg}
-          rating={item.stats.rating}
-          reviewCount={item.stats.reviewCount}
-          location={item.location}
-          title={item.title}
-          price={item.price}
-        />
+        key={item.id}
+        item={item}
+          // img={item.coverImg}
+          // rating={item.stats.rating}
+          // reviewCount={item.stats.reviewCount}
+          // location={item.location}
+          // title={item.title}
+          // price={item.price}
+          // openSpots={item.openSpots}
+      />
     )
   })
   return (
--- project-2/src/components/Card.js
+++ project-2/src/components/Card.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 function Card(props) {
+    let badgeText;
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    }else if ( props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return (
         <div className="card">
-            <img src={`assets/images/${props.img}`} alt="" className="card--image" />
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`assets/images/${props.item.coverImg}`} alt="" className="card--image" />
             <div className="card--stats">
                 <img src="assets/images/star.png" alt="" className="card--star"/>
-                <span>{ props.rating }</span>
-                <span className="gray">({ props.reviewCount }) • </span>
-                <span className="gray">{ props.location }</span>
+                <span>{ props.item.stats.rating }</span>
+                <span className="gray">({ props.item.stats.reviewCount }) • </span>
+                <span className="gray">{ props.item.location }</span>
             </div>
-            <p>{ props.title }</p>
-            <p><span className="bold">From ${ props.price }</span> / person</p>
+            <p>{ props.item.title }</p>
+            <p><span className="bold">From ${ props.item.price }</span> / person</p>
         </div>
     )
 }
