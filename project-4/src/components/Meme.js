5603b1b6edd2d6891d0b6cb50b508e700c7cd3e3
yongchang he <hyc0812@163.com>
2022-06-07 12:08:05 -0600

    update

--- project-4/src/components/Meme.js
+++ project-4/src/components/Meme.js
@@ -1,13 +1,32 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    // Get a random url. This method is really elegant and important!!
+    function getMemeImage() {
+       const memesArray = memesData.data.memes;
+       const radomNumber = Math.floor(Math.random() * memesArray.length);
+       const url = memesArray[radomNumber].url;
+       console.log(url);
+    }
+    // need to change the form element into div element
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input type="text" placeholder="Bottom text" className="form--input"/>
-                <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button className="form--button" onClick={ getMemeImage }>Get a new meme image 🖼</button>
+            </div>
         </main>
     )
 }
+
+// "memes": [
+//     {
+//         "id": "181913649",
+//         "name": "Drake Hotline Bling",
+//         "url": "https://i.imgflip.com/30b1gx.jpg",
+//         "width": 1200,
+//         "height": 1200,
+//         "box_count": 2
+//     },
