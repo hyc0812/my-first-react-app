c1c03234435875e4f37f469f49e2e2217b8f66e9
yongchang he <hyc0812@163.com>
2022-06-07 11:33:44 -0600

    update

--- mini-exercise-1/src/App.js
+++ mini-exercise-1/src/App.js
@@ -4,10 +4,14 @@ import React from "react"
 export default function App() {
     function handleClick() {
         console.log("I was clicked!")
-    }   
+    } 
+    // add console.log when mouse hover the image
+    function handleOnMouseOver() {
+        console.log("MouseOver")
+    }  
     return (
         <div className="container">
-            <img src="https://picsum.photos/640/360" alt=""/>
+            <img src="https://picsum.photos/640/360" alt="" onMouseOver={ handleOnMouseOver }/>
             {/* <img src="assets/images/mountain-bike.png" alt="" /> */}
             <button onClick={ handleClick }>Click me</button>
         </div>
