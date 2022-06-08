84b73a193eb48a04f917008ae79ae5748af75f3f
yongchang he <hyc0812@163.com>
2022-06-08 14:21:14 -0600

    update

--- mini-exercise-3/src/App.js
+++ mini-exercise-3/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import boxes from './boxes';
+import Box from './Box';
 import './App.css';
 
-function App() {
+function App(props) {
   // Initialize the state
   const [squares, setSquares] = React.useState(boxes);
-  // Map
+  // Map the state array
   const squareElements = squares.map(square => (
-    <div className='box' key={square.id}>{square.id}</div>
+    <Box key={square.id} on={square.on}/>
   ))
-  console.log(squareElements[0])
+
   return (
     <main>
       {squareElements}
@@ -29,4 +30,15 @@ export default App;
      * 2. Map over that state array and display each one
      *    as an empty square (black border, transparent bg color)
      *    (Don't worry about using the "on" property yet)
+     */
+
+    /**
+     * Challenge part 2:
+     * 1. Create a separate component called "Box" and
+     *    replace the `div` above with our <Box /> components
+     * 2. Pass the Box component a prop called `on` with the
+     *    value of the same name from the `boxes` objects
+     * 3. In the Box component, apply dynamic styles to determine
+     *    the backgroundColor of the box. If it's `on`, set the
+     *    backgroundColor to "#222222". If off, set it to "none"
      */
---
+++ mini-exercise-3/src/Box.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Box(props) {
+    console.log(props.on)
+    return (
+        <div className='box'></div>
+    )
+}
