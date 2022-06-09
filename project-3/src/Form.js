5a5c38f96ae3ed5f3dbf52f5b1685c11eac912ed
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-09 14:16:44 -0600

    update

--- project-3/src/Form.js
+++ project-3/src/Form.js
@@ -2,24 +2,36 @@ import React from "react";
 
 export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName:"", lastName:"", email:"", comments:""}
+        {
+            firstName:"",
+            lastName:"",
+            email:"",
+            comments:"",
+            isFriendly: true,
+            employment:"unemployed",
+            favColor:""
+        }
     );
     
-    console.log(formData)
-
-    
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormData(preFormData => {
             return {
                 ...preFormData,
-                [event.target.name]:event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     };
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        // submitToWhateverApi(formData);
+        console.log(formData);
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <input 
                     type="text"
                     placeholder="First Name"
@@ -47,7 +59,70 @@ export default function Form() {
                     name="comments"
                     value={ formData.comments }
                 />
+                <input 
+                    type="checkbox"
+                    id="isFriendly"
+                    checked={ formData.isFriendly }
+                    name="isFriendly"
+                    onChange={ handleChange }
+                />
+                <label htmlFor="isFriendly">Are you friendly?</label>
+                <br />
+                <br />
+                
+                <fieldset>
+                    <legend>Current employment status</legend>
 
+                    <input
+                        type="radio"
+                        id="unemployed"
+                        name="employment"
+                        value="unemployed"
+                        onChange={ handleChange }
+                    />
+                    <label htmlFor="unemployed">Unemployed</label>
+                    <br />
+
+                    <input
+                        type="radio"
+                        id="part-time"
+                        name="employment"
+                        value="part-time"
+                        onChange={ handleChange }
+                    />
+                    <label htmlFor="part-time">Part-time</label>
+                    <br />
+
+                    <input
+                        type="radio"
+                        id="full-time"
+                        name="employment"
+                        value="full-time"
+                        onChange={ handleChange }
+                    />
+                    <label htmlFor="full-time">Full-time</label>
+                    <br />
+                </fieldset>
+
+                <label htmlFor="favColor">What is your favorite color?</label>
+                <br />
+                <select id="favColor"
+                        name="favColor" 
+                        value={formData.favColor}
+                        onChange={ handleChange }>
+                    <option value="">-Choose-</option>
+                    <option value="red">Red</option>
+                    <option value="orange">Orange</option>
+                    <option value="yellow">Yellow</option>
+                    <option value="green">Green</option>
+                    <option value="blue">Blue</option>
+                    <option value="indigo">Indigo</option>
+                    <option value="violet">Violet</option>
+                </select>
+                <br />
+                <br />
+                <button>Submit</button>  
+                {/* type="submit" by default in React.js*/}
             </form>
         </div>
     );
