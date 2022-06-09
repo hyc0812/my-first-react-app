8c2831c35150df4fc76802403953257f1e7af7d0
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-09 10:58:56 -0600

    update

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -3,6 +3,7 @@ import Joke from "./Joke"
 import jokesData from "./jokesData"
 import Unread from "./Unread"
 import Form from "./Form"
+import "./App.css"     //   remember to check this point if you found that the css style is not displayed!
 
 export default function App() {
     const jokeElements = jokesData.map(joke => {
