0435f40f9966452c1b8e7538beab43500cfea35c
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-08 23:45:33 -0600

    update alternative return method

--- project-3/src/Unread.js
+++ project-3/src/Unread.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default function Unread() {
-    const [messages, setMessages] = React.useState(["a", "b", "c", "d"])
+    const [messages, setMessages] = React.useState(["a", "b", "c", "d", "e"])
 
     console.log(messages)
     return (
@@ -9,7 +9,10 @@ export default function Unread() {
             {
                 messages.length === 0 ?
                 <h1>You are all caught up!</h1> :
-                <h1>you have {messages.length} unread message{messages.length > 1 && "s"}</h1>
+                // <h1>you have {messages.length} unread message{messages.length > 1 && "s"}</h1>
+                <h1>You have {messages.length} unread {
+                 messages.length > 1 ? "messages" : "message"   
+                }</h1>
             }
         </div>
     )
