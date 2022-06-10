80b8b123adf7b22e688b3d73a91ad1f5878ade5c
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-10 11:19:32 -0600

    update

--- mini-exercise-6/src/App.js
+++ mini-exercise-6/src/App.js
@@ -1,10 +1,25 @@
 
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [starWarsDate, setStarWarsData] = React.useState({})
+  const [count, setCount] = React.useState(1)
+
+  React.useEffect(function() {
+    console.log("Effect ran");
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then(res => res.json())
+      .then(data => setStarWarsData(data))
+  },[count])
+
   return (
-    <div className="App">
-    Hello
+    <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount(
+        prevCount => prevCount + 1
+      )}>Get next character</button>
+      <pre>{JSON.stringify(starWarsDate, null, 2)}</pre>
     </div>
   );
 }
