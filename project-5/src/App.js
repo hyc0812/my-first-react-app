1a7652ce4074d1aca57a925ca7cedd68ce488038
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-11 21:12:40 -0600

    update

--- project-5/src/App.js
+++ project-5/src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import React from 'react';
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true);
+  function toggleDarkMode() {
+    setDarkMode(preMode => !preMode)
+  }
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={ darkMode } toggleDarkMode={ toggleDarkMode }/>
+      <Main darkMode={ darkMode }/>
     </div>
   );
 }
