28b3f64f777beec8463aa1ae4a537f0710b55540
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-12 14:46:36 -0600

    update

--- project-7/src/App.js
+++ project-7/src/App.js
@@ -4,9 +4,8 @@ import React from "react";
 import Die from './Die';
 
 
-
 function App() {
-
+  const [dice, setDice] = React.useState(allNewDice());
   function allNewDice () {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -14,20 +13,12 @@ function App() {
     }
     return newDice;
   }
-  console.log(allNewDice())
+
+  const diceElements = dice.map(die => <Die value={die} />);
   return (
     <main>
       <div className='dice-container'>
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="7" />
-        <Die value="8" />
-        <Die value="9" />
-        <Die value="1" />
+        { diceElements }
       </div>
     </main>
   );
