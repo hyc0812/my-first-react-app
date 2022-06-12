620c1a990b6f086155a3003fd239e1d0eced8098
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-12 14:38:04 -0600

    update

--- project-7/src/App.js
+++ project-7/src/App.js
@@ -1,11 +1,35 @@
 
 import './App.css';
+import React from "react";
+import Die from './Die';
+
+
 
 function App() {
+
+  function allNewDice () {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random()* 6));
+    }
+    return newDice;
+  }
+  console.log(allNewDice())
   return (
-    <div className="App">
-      hello
-    </div>
+    <main>
+      <div className='dice-container'>
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="1" />
+      </div>
+    </main>
   );
 }
 
