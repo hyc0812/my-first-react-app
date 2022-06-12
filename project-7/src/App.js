5ccbc60eb9438139bd1f995a9aa5050c65bc8ad2
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-06-12 16:41:18 -0600

    update

--- project-7/src/App.js
+++ project-7/src/App.js
@@ -7,20 +7,34 @@ import {nanoid} from "nanoid";
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random()* 6),
+      isHeld: false, 
+      id: nanoid()
+    }
+  }
+
   function allNewDice () {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random()* 6),
-        isHeld: false, 
-        id: nanoid()
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
   function rollDice () {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die =>{
+      return die.isHeld ? die : generateNewDie()
+    }))
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ?
+        {...die, isHeld: !die.isHeld} : die
+    }))
   }
 
   const diceElements = dice.map(die =>  
@@ -28,6 +42,9 @@ function App() {
       key={die.id} 
       value={die.value} 
       isHeld={die.isHeld}
+      holdDice={() =>
+        holdDice(die.id)
+      }
     />);
   return (
     <main>
